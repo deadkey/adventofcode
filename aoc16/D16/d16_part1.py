807b44ba442a01e9e2667a31b9159248d72091e6
Maj Stenmark <majstenmark@gmail.com>
2020-12-15 15:47:00 +0100

    2016 day 16

--- aoc16/D16/d16_part1.py
+++ aoc16/D16/d16_part1.py
@@ -17,13 +17,36 @@ def db(*a):
 
 def parse(line):
     return lazy_ints(line.split())
-    
+
+def gen(a):
+    b = a[::-1]
+    flipb = ['1' if bit == '0' else '0' for bit in b]
+    c = a + '0' + ''.join(flipb)
+    return c
+
+def pairwise(T):
+    ch= []
+    for s in range(0, len(T), 2):
+        if T[s] == T[s+1]:
+            ch.append('1')
+        else:
+            ch.append('0')
+    return ch
+
+def checksum(T):
+    while len(T) % 2 == 0:
+        T = pairwise(T)
+    return ''.join(T)
 
 def p1(v):
     lines = v.strip().split('\n')
-    cnt = 0
-    data = [parse(line) for line in lines]
-    return cnt
+    a = lines[0]
+    L = 272
+    while len(a) < L:
+        a = gen(a)
+    return checksum(a[0:L])
+
+
 
 def p2(v):
     return p1(v)
@@ -35,6 +58,6 @@ def manual():
         
 cmds, stats, io, so, DB = get_args(sys.argv)    
 if not io: run_samples(p1, p2, cmds)
-if not so: run(get_year(),  get_day(), p1, p2, cmds)
+if not so: run(2016,16, p1, p2, cmds)
 if stats: print_stats()
 #manual()
