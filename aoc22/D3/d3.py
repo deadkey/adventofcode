393ed753229224d0c0dc2e833fcbf95ed19c5c9e
Maj Stenmark <majstenmark@gmail.com>
2022-12-03 06:19:47 +0100

    day 3

---
+++ aoc22/D3/d3.py
@@ -0,0 +1,81 @@
+import sys, time
+from datetime import date
+sys.path.extend(['..', '.'])
+from collections import *
+from fetch import *
+from util import *
+import math
+from collections import defaultdict as dd, Counter
+#import drawgraph #only works in python3
+#lo, hi, lt, pw = lazy_ints(multisplit(line, '-: ')) #chars only!
+#or lo, hi, lt, pw = lazy_ints(multisplit(line, ['-',': ','))
+
+#Run with command p3 setup.py 
+# to setup everything
+# then cd to day folder
+#Run with command p3 d30.py p1 submit stat
+# io = input only
+# so = sample only
+
+import re
+#use regex re.split(' |,|: ', line)
+
+def db(*a): 
+    if DB: print(*a)
+
+#crazy input, use multisplit? 
+def parse(line):
+    #return lazy_ints(multisplit(line, '')) 
+    return lazy_ints(line.split())
+
+def inall(ch, allset):
+    for s in allset:
+        if ch not in s: return False
+    return True
+
+def p1(v):
+    lines = v.strip().split('\n')
+    chunks = tochunks(v)
+    data = [parse(line) for line in lines]
+    su = 0
+    for i in range(len(data)):
+        d = data[i]
+        k = len(d)//2
+        first = set(d[0:k])
+        second = set(d[k:])
+       
+        for ch in first:
+            prio = 0
+            if ch in second:
+                db(ch)
+                if ch.islower():
+                    db('lower')
+                    prio += ord(ch) - ord('a') + 1
+                    db(prio)
+                else:
+                    prio += ord(ch) - ord('A') + 27
+            su += prio
+
+
+    return su
+
+
+def p2(v):
+    return p1(v)
+
+
+def manual():
+    v = open("real.txt", 'r').read().strip('\n')
+    print('part_1: {}\npart2: {}'.format(p1(v), p2(v)))
+        
+cmds, stats, io, so, DB = get_args(sys.argv)
+if not io: run_samples(p1, p2, cmds)
+
+if 'manual' in cmds: 
+    manual()
+    exit()
+
+if not so: run(2022,3, p1, p2, cmds)
+if stats: print_stats()
+
+#manual()
