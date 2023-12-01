d0e17e5bf0e34083952ac82f206147e1ce39efde
Maj Stenmark <majstenmark@gmail.com>
2023-12-01 06:24:55 +0100

    Setup for 2023, day 1 and updated reqs

---
+++ aoc23/template.py
@@ -0,0 +1,60 @@
+import sys, time
+from datetime import date
+sys.path.extend(['..', '.'])
+from collections import *
+from fetch import *
+from util import *
+import math
+from collections import defaultdict as dd, Counter
+#import drawgraph #only works in python3
+#lo, hi, lt, pw = lazy_ints(multisplit(line, '-: ')) #chars only!
+#or lo, hi, lt, pw = lazy_ints(multisplit(line, ['-',': ','))
+
+#Run with command p3 setup.py 
+# to setup everything
+# then cd to day folder
+#Run with command p3 d30.py p1 submit stat
+# io = input only
+# so = sample only
+
+import re
+#use regex re.split(' |,|: ', line)
+
+def db(*a): 
+    if DB: print(*a)
+
+#crazy input, use multisplit? 
+def parse(line):
+    #return lazy_ints(line.split())
+    return lazy_ints(multisplit(line, ' ')) 
+    
+
+def p1(v):
+    lines = v.strip().split('\n')
+    chunks = tochunks(v)
+    data = [parse(line) for line in lines]
+    su = 0
+    for i in range(len(data)):
+        d = data[i]
+
+    return su
+
+def p2(v):
+    return p1(v)
+
+
+def manual():
+    v = open("real.txt", 'r').read().strip('\n')
+    print('part_1: {}\npart2: {}'.format(p1(v), p2(v)))
+        
+cmds, stats, io, so, DB = get_args(sys.argv)
+if not io: run_samples(p1, p2, cmds)
+
+if 'manual' in cmds: 
+    manual()
+    exit()
+
+if not so: run(get_year(),  get_day(), p1, p2, cmds)
+if stats: print_stats()
+
+#manual()
---
+++ aoc23/util.py
@@ -0,0 +1,181 @@
+import requests
+from secret import session
+import os, glob, time
+from datetime import datetime
+import bs4
+import logging as log
+import re
+
+
+from itertools import chain, combinations
+
+def powerset(iterable):
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
+
+def isint(i):
+    try:
+        int(i)
+        return True
+    except:
+        return False
+
+
+def removeall(s, *arg):
+    for a in arg:
+        s = s.replace(a, '')
+    return s
+
+
+def int_convert(s):
+    if isint(s):
+        return int(s)
+    return s
+
+INF = 10**30
+def togrid(lines): 
+    grid = [list(line) for line in lines]
+    return grid
+
+def tointgrid(lines): 
+    grid = [list(map(int, line.split())) for line in lines]
+    return grid, len(grid), len(grid[0])
+
+
+def copygrid(grid):
+    R = len(grid)
+    nxt =[]
+    for r in range(R):
+        nxt.append(list(grid[r]))
+    return nxt
+
+def emptygrid(R, C, val):
+    return [[val] * C for r in range(R)]
+
+def cntgrid(grid, val):
+    return sum(grid[r].count(val) for r in range(len(grid)))
+
+
+def get4nb(r, c, rmin = -INF, rmax = INF, cmin = -INF, cmax = INF):
+    diff = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+    nb = []
+    for dr, dc in diff:
+        if rmin <= r + dr < rmax and cmin <= c + dc < cmax:
+            nb.append((r+dr, c + dc))
+    return nb 
+
+def grid4nb(r, c, grid):
+    rmin = 0
+    cmin = 0
+    rmax = len(grid)
+    cmax = len(grid[0])
+    diff = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+    nb = []
+    for dr, dc in diff:
+        if rmin <= r + dr < rmax and cmin <= c + dc < cmax:
+            nb.append((r+dr, c + dc))
+    return nb     
+
+def get8nb(r, c, rmin = -INF, rmax = INF, cmin = -INF, cmax = INF):
+    diff = [(-1, 0), (1, 0), (0, 1), (0, -1), (-1, -1), (1, -1), (-1, 1), (1, 1)]
+    nb = []
+    for dr, dc in diff:
+        if rmin <= r + dr < rmax and cmin <= c + dc < cmax:
+            nb.append((r+dr, c + dc))
+    return nb  
+
+
+def grid8nb(r, c, grid):
+    rmin = 0
+    cmin = 0
+    rmax = len(grid)
+    cmax = len(grid[0])
+    diff = [(-1, 0), (1, 0), (0, 1), (0, -1), (-1, -1), (1, -1), (-1, 1), (1, 1)]
+    nb = []
+    for dr, dc in diff:
+        if rmin <= r + dr < rmax and cmin <= c + dc < cmax:
+            nb.append((r+dr, c + dc))
+    return nb     
+
+def printgrid(grid):
+    for r in range(len(grid)):
+        out = ''.join(map(str, grid[r]))
+        print(out)
+
+def printdictxy(grid):
+    INF = 10**12
+    minx = min(grid.keys())[0]
+    maxx = max(grid.keys())[0]
+    
+    miny = min(grid.keys(), key = lambda x: x[1])[1]
+    maxy = max(grid.keys(), key = lambda x: x[1])[1]
+   
+    for y in range(miny, maxy + 1):
+        out = []
+        for x in range(minx, maxx + 1):   
+            p = '#' if grid[x, y] == 1 else ' '
+            out.append(p) 
+        print(''.join(out))
+
+def printdictrc(grid):
+    INF = 10**12
+    minx = min(grid.keys())[0]
+    maxx = max(grid.keys())[0]
+    
+    miny = min(grid.keys(), key = lambda x: x[1])[1]
+    maxy = max(grid.keys(), key = lambda x: x[1])[1]
+   
+    for x in range(minx, maxx + 1):
+        out = []
+        for y in range(miny, maxy + 1):   
+            p = '#' if grid[x, y] == 1 else ' '
+            out.append(p) 
+        print(''.join(out))
+
+def multisplit(s, schars):
+    out = [s]
+    for delim in schars:
+        newout = []
+        for word in out:
+            spl = word.split(delim)
+            newout.extend(spl)
+        out = newout
+    #reg = '|'.join(schars)
+    #out =  re.split(reg, s)
+    out = list(map(lambda x: x.strip(), out))
+    out = filter(lambda x: len(x.strip())> 0, out)
+
+    return out
+
+def tochunks(v):
+    ch = v.split('\n\n')
+    lines = []
+    for c in ch:
+        lines.append(c.split('\n'))
+    return lines
+
+
+def lazy_ints(li):
+    li = list(map(int_convert, li)) 
+    if len(li) == 1:
+        return li[0]
+    return li
+
+def mul(li):
+    p = 1
+    for v in li:
+        p *= v
+    return p
+
+def add(li):
+    ints = filter(lambda x: isint(x), li)
+    return sum(ints)
+
+
+def print_stats():
+
+    v = open("real.txt", 'r').read().strip('\n')
+    lines = len(v.strip().split('\n'))
+    print('Len input: {} lines {} chars'.format(lines, len(v)))
+    
